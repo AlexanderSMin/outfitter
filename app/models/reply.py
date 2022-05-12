d3cd0d942be73f2716d9bf12b3048912d4b68bc3
Alex Min <alexmin@2424@yahoo.com>
2022-05-11 22:57:16 -0700

    reply and post models added

---
+++ app/models/reply.py
@@ -0,0 +1,30 @@
+from .db import db
+from .user import User
+
+class Reply(db.Model):
+    __tablename__ = 'replies'
+
+    id = db.Column(db.Integer, primary_key=True)
+    post_id = db.Column(db.Integer(), db.ForeignKey('posts.id'))
+    user_id = db.Column(db.Integer(),db.ForeignKey('users.id'), nullable=False)
+    body= db.Column(db.String(255), nullable=False)
+
+    comment_post=db.relationship('User', backref='comments', lazy=True)
+
+    def to_dict(self):
+        user = User.query.get(self.user_id)
+
+        return {
+            "id": self.id,
+            "photo_id": self.photo_id,
+            "user_id": self.user_id,
+            "body": self.body,
+            "username": user.username
+        }
+
+    def edit_comment(self, body):
+        self.body = body
+        return body
+
+    user = db.relationship("User", back_populates="replies")
+    photos = db.relationship("Photo", back_populates="replies")
