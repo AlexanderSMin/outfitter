50db09d4612bb56d7dd9eeb9df19f9bcb3a1a721
Alex Min <alexmin@2424@yahoo.com>
2022-05-22 18:38:14 -0700

    some styling

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -19,11 +19,19 @@ def username_exists(form, field):
     user = User.query.filter(User.username == username).first()
     if user:
         raise ValidationError('Username is already in use.')
+    if len(username) < 3 or len(username) > 40:
+        raise ValidationError('Username must be between 3 and 40 characters')
+
+
+# def firstName_check(field):
+#     firstName= field.data
+#     if len(firstName) > 50:
+#         raise ValidationError('Provided first name too long')
 
 
 class SignUpForm(FlaskForm):
     first_name = StringField(
-        'first_name', validators=[DataRequired(message='Please provide a first name')]
+        'first_name', validators=[DataRequired(message='Please provide a first name'),]
     )
     last_name = StringField(
         'last_name', validators=[DataRequired(message='Please provide a last name')]
